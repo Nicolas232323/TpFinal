0a4183b56b13ddacb7b6e300394247ab2ebcedac
NICOLAS GABRIEL SIMONE <47805665@ort>
2023-12-01 09:01:59 -0300

    A

--- ProyectoFinal/obj/Debug/net7.0/ProyectoFinal.AssemblyInfo.cs
+++ ProyectoFinal/obj/Debug/net7.0/ProyectoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37628790eccd9202811f90efeb8805e6d7b539b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
